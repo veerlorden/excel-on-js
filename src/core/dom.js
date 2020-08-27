af75335fc3cd70206fdbe8a8936963e35d826e18
Vadim Burkin <C:\Users\veerl\AppData\Roaming\The Bat!>
2020-08-27 20:47:57 +0300

    fixes

--- src/core/dom.js
+++ src/core/dom.js
@@ -93,11 +93,13 @@ class Dom {
   }
 
   addClass(className) {
-    return this.$el.classList.add(className)
+    this.$el.classList.add(className)
+    return this
   }
 
   removeClass(className) {
-    return this.$el.classList.remove(className)
+    this.$el.classList.remove(className)
+    return this
   }
 }
 
